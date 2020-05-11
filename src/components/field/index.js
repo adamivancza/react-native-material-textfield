7c87bcb04111ad0e2a9bdc053f10255197ef6379
Adam Ivancza <adam@getcodified.com>
2020-05-11 14:50:52 +0200

    Setting useNativeDriver to false

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -231,6 +231,7 @@ export default class TextField extends PureComponent {
     let options = {
       toValue: this.focusState(),
       duration,
+      useNativeDriver: false,
     };
 
     startAnimation(focusAnimation, options, this.onFocusAnimationEnd);
